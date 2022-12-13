c9953c6a163baf0789d3652890594a917768003f
xzclouds <xcloudsgls@gmail.com>
2022-12-13 18:57:26 -0500

    Añadí sesion 2

---
+++ sesiones/sesion 2/University/UniversityApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniversityApi.Models;
+
+namespace UniversityApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Users>? Users { get; set; }
+    }
+}
