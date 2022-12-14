a65e896de70e6d5afa016465199884238596ade9
xzclouds <xcloudsgls@gmail.com>
2022-12-14 10:27:06 -0500

    Añadí ejercicio 2

---
+++ ejercicios/ejercicio 2/University/UniversityApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniversityApi.Models;
+
+namespace UniversityApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category>? Categories { get; set; }
+        public DbSet<Chapter>? Chapters { get; set; }
+        public DbSet<Course>? Courses { get; set; }
+        public DbSet<Student>? Students { get; set; }
+        public DbSet<User>? Users { get; set; }
+    }
+}
