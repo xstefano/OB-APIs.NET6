9e68a0d6f669af2a70e81c01e30cab4d909be665
xzclouds <xcloudsgls@gmail.com>
2022-12-19 22:55:22 -0500

    Añadí ejercicio 3

---
+++ ejercicios/ejercicio 3/University/LinqSnippets/Services.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniversityApi.Data;
+using UniversityApi.Models;
+
+namespace LinqSnippets
+{
+    public class Services
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Services(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        // Buscar usuarios por email:
+        public async void FindUsersByEmail(string email)
+        {
+            var users = await _context.Users.ToListAsync();
+
+            var findUsers = users
+                                .Select(u => u)
+                                .Where(u => u.EmailAddress == email)
+                                .OrderBy(u => u.Name);
+        }
+
+        // Buscar alumnos mayores de edad:
+
+        public async void FindStudentsLegalAge()
+        {
+            var students = await _context.Students.ToListAsync();
+
+            var findStudentsLegalAge = students
+                                              .Select(s => s)
+                                              .Where(s => (DateTime.Now.Year - s.Dob.Year) >= 18)
+                                              .OrderBy(s => s.FirstName);
+        }
+
+        // Buscar alumnos que tengan al menos un curso:
+
+        public async void FindStudentsWithMoreThanOneCourse()
+        {
+            var students = await _context.Students.ToListAsync();
+
+            var studentsWithMoreThanOneCourse = students
+                                                       .Select(s => s)
+                                                       .Where(s => s.Courses.Count > 0)
+                                                       .OrderBy(s => s.FirstName);
+        }
+
+        // Buscar cursos de un nivel determinado que al menos tengan un alumno inscrito:
+
+       public async void FindCoursesOfLevel(Level level)
+       {
+            var courses = await _context.Courses.ToListAsync();
+
+            var coursesOfLevel = courses
+                                       .Select(c => c)
+                                       .Where(c => (c.Level == level) && (c.Students.Count > 0))
+                                       .OrderBy(c => c.Name);
+       }
+
+        // Buscar cursos de un nivel determinado que sean de una categoría determinada:
+
+        public async void FindCoursesOfLevelAndCategory(Level level, Category category)
+        {
+            var courses = await _context.Courses.ToListAsync();
+
+            var coursesOfLevelAndCategory = courses
+                                                  .Select(c => c)
+                                                  .Where(c => (c.Level == level) && (c.Categories == category && c.Categories.Count > 0))
+                                                  .OrderBy(c => c.Name);
+        }
+
+        // Buscars cursos sin alumnos:
+        
+        public async void FindCoursesWithoutStudents()
+        {
+            var courses = await _context.Courses.ToListAsync();
+
+            var coursesWithoutStudents = courses
+                                               .Select(c => c)
+                                               .Where(c => c.Students.Count < 1)
+                                               .OrderBy(c => c.Name);                                  
+        }
+    }
+}
---
+++ ejercicios/ejercicio 3/University/UniversityApi/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityApi.Models
+{
+    public class Category : BaseEntity
+    {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public ICollection<Course> Courses { get; set; } = new List<Course>();
+    }
+}
