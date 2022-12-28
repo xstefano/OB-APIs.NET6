698c93edfcb0c3f0dc5324ea85b27f2c1f9be53b
xzclouds <xcloudsgls@gmail.com>
2022-12-28 18:07:29 -0500

    Añadí sesion 8

---
+++ sesiones/sesion 8/University/UniversityApi/Program.cs
@@ -0,0 +1,96 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using UniversityApi;
+using UniversityApi.Data;
+using UniversityApi.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// TODO: Connection with SQL Server Express.
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add Services of JWT Autorization:
+builder.Services.AddJwtTokenServices(builder.Configuration);
+
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Add Custom Services (folder Services)
+builder.Services.AddScoped<IStudentsService, StudentsService>();
+
+// TODO: Add the rest of services:
+
+// Add Authorization:
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("UserOnlyPolicy", policy => policy.RequireClaim("UserOnly", "User1"));
+});
+
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+// TODO: Config Swagger to take care of Autorization of JWT:
+
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization Header using Bearer Scheme"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+			new OpenApiSecurityScheme
+		        {
+			        Reference = new OpenApiReference
+			        {
+				        Type = ReferenceType.SecurityScheme,
+				        Id = "Bearer"
+			        }
+		        },
+		    new string[]{ }
+		}
+    });
+});
+
+// CORS Configuration
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "CorsPolicy", builder =>
+    {
+        builder.AllowAnyOrigin();
+        builder.AllowAnyMethod();
+        builder.AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+// Tell app to user CORS
+app.UseCors("CorsPolicy");
+
+app.Run();
