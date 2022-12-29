341a127fbf0f49ea594218f2d98a3e8ba750ede4
xzclouds <xcloudsgls@gmail.com>
2022-12-28 20:13:16 -0500

    Añadí ejercicio 8

---
+++ ejercicios/ejercicio 8/APIVersionControl/APIVersionControl/Controllers/V1/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using APIVersionControl.DTO;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace APIVersionControl.Controllers.V1
+{
+	[ApiVersion("1.0")]
+	[Route("api/v{version:apiVersion}[controller]")]
+	[ApiController]
+	public class ProductsController : ControllerBase
+	{
+		private const string ApiTestURL = "https://fakestoreapi.com/products";
+		private readonly HttpClient _httpClient;
+
+		public ProductsController(HttpClient httpClient)
+		{
+			_httpClient = httpClient;
+		}
+
+		[MapToApiVersion("1.0")]
+		[HttpGet(Name = "GetProductsData")]
+		public async Task<IActionResult> GetProducts()
+		{
+			var response = await _httpClient.GetStreamAsync(ApiTestURL);
+			var productsData = await JsonSerializer.DeserializeAsync
+												<List<ProductsWithRating>>(response);
+
+			return Ok(productsData);
+		}
+	}
+}
---
+++ ejercicios/ejercicio 8/APIVersionControl/APIVersionControl/Controllers/V2/ProductsControllerV2.cs
@@ -0,0 +1,30 @@
+﻿using APIVersionControl.DTO;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace APIVersionControl.Controllers.V2
+{
+	[ApiVersion("2.0")]
+	[Route("api/v{version:apiVersion}[controller]")]
+	[ApiController]
+	public class ProductsController : ControllerBase
+	{
+		private const string ApiTestURL = "https://fakestoreapi.com/products";
+		private readonly HttpClient _httpClient;
+
+		public ProductsController(HttpClient httpClient)
+		{
+			_httpClient = httpClient;
+		}
+
+		[MapToApiVersion("2.0")]
+		[HttpGet(Name = "GetProductsProducts")]
+		public async Task<IActionResult> GetProductsDataASync()
+		{
+			var response = await _httpClient.GetStreamAsync(ApiTestURL);
+			var productsData = await JsonSerializer.DeserializeAsync
+												<List<ProductsWithoutRating>>(response);
+			return Ok(productsData);
+		}
+	}
+}
