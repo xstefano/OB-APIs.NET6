30776d46a2211c4be5390a91e39af5768327bf6c
xzclouds <xcloudsgls@gmail.com>
2022-12-28 19:04:01 -0500

    Añadí sesion 9

---
+++ sesiones/sesion 9/APIVersionControl/APIVersionControl/Controllers/V2/UsersControllerV2.cs
@@ -0,0 +1,37 @@
+﻿using APIVersionControl.DTO;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace APIVersionControl.Controllers.V2
+{
+	[ApiVersion("2.0")]
+	[Route("api/v{version:apiVersion}[controller]")]
+	[ApiController]
+	public class UsersController : ControllerBase
+	{
+		private const string ApiTestURL = "https://dummyapi.io/data/v1/user?limit=30";
+		private const string AppID = "6283561db7f84b13fae4115f";
+		private readonly HttpClient _httpClient;
+
+		public UsersController(HttpClient httpClient)
+		{
+			_httpClient = httpClient;
+		}
+
+		[MapToApiVersion("2.0")]
+		[HttpGet(Name = "GetUsersData")]
+		public async Task<IActionResult> GetUsersDataASync()
+		{
+			_httpClient.DefaultRequestHeaders.Clear();
+			_httpClient.DefaultRequestHeaders.Add("app-id", AppID);
+
+			var response = await _httpClient.GetStreamAsync(ApiTestURL);
+
+			var usersData = await JsonSerializer.DeserializeAsync<UsersResponseData>(response);
+
+			var users = usersData?.data;
+
+			return Ok(users);
+		}
+	}
+}
---
+++ sesiones/sesion 9/APIVersionControl/APIVersionControl/DTO/UserData.cs
@@ -0,0 +1,21 @@
+﻿namespace APIVersionControl.DTO
+{
+	public class User
+	{
+		public string? Id { get; set; }
+		public string? Name { get; set; }
+
+		public string? FirstName { get; set; }
+		public string? LastName { get; set; }
+		public string? Picture { get; set; }
+
+	}
+
+	public class UsersResponseData
+	{
+		public User[] data { get; set; }
+		public int total { get; set; }
+		public int page { get; set; }
+		public int limit { get; set; }
+	}
+}
